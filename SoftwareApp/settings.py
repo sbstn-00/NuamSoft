a4ccf513cb76bd1fa7f3c990281ecf4381878f85
Sebastian <sebastiancopa45@gmail.com>
2025-11-11 00:37:06 -0300

    Fix: Reestructured DB config to handle ENGINE outside dj_database_url.config()

--- SoftwareApp/settings.py
+++ SoftwareApp/settings.py
@@ -79,13 +79,16 @@ WSGI_APPLICATION = 'SoftwareApp.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-  'default': dj_database_url.config(
-        default=os.environ.get('MYSQL_URL'),
-        
-        ENGINE='django.db.backends.mysql', 
-        conn_max_age=600,
-  ) 
+    'default': dj_database_url.config(
+        default=os.environ.get('MYSQL_URL')
+    )
 }
+
+# 1. Aseguramos que la configuración existe antes de modificarla
+if DATABASES['default']:
+    # 2. Añadimos o sobrescribimos las claves adicionales al diccionario resultante:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.mysql' 
+    DATABASES['default']['CONN_MAX_AGE'] = 600
 
 
 AUTH_PASSWORD_VALIDATORS = [
